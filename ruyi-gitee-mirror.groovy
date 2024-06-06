065a10f7847af248e9e2177761b4990313969298
weilinfox <caiweilin@iscas.ac.cn>
2024-06-06 18:33:03 +0800

    It should work

--- ruyi-gitee-mirror.groovy
+++ ruyi-gitee-mirror.groovy
@@ -1,15 +1,20 @@
 
 pipeline {
-    agent none
+    agent any
 
     stages {
-        stage('Hello') {
+        stage('Sync gitee repo') {
             environment {
                 GITEE_API_TOKEN = credentials('GITEE_API_TOKEN_FOR_RUYISDK_GITEE_MIRROR')
+                GITEE_REPO = 'ruyisdk/ruyi-gitee-mirror'
+                GITHUB_REPO = "https://github.com/weilinfox/ruyi-gitee-mirror.git"
             }
 
             steps {
-                sh 'env'
+                sh 'git clone https://${GITEE_APT_TOKEN_USR}:${GITEE_API_TOKEN_PSW}@gitee.com/${GITEE_REPO}.git orig --depth=1'
+                sh 'cd orig && git pull https://github.com/weilinfox/ruyi-gitee-mirror.git'
+                sh 'cd orig && git push'
+                sh 'cd orig && git push --tags'
             }
         }
     }
