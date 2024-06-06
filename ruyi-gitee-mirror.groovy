bc906416a8135244cde62983996623b2aa16204b
weilinfox <caiweilin@iscas.ac.cn>
2024-06-06 18:12:47 +0800

    Change credentials name
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- ruyi-gitee-mirror.groovy
+++ ruyi-gitee-mirror.groovy
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('Hello') {
             environment {
-                GITEE_API_TOKEN = credentials('d7f8773e-2dd1-4d11-b723-11365c61e0a4')
+                GITEE_API_TOKEN = credentials('GITEE_API_TOKEN_FOR_RUYISDK_GITEE_MIRROR')
             }
 
             steps {
