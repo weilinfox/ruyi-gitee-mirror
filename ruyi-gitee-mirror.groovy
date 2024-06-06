227d9df8e899239047341bde619d90c5aaf205cc
weilinfox <caiweilin@iscas.ac.cn>
2024-06-06 18:51:05 +0800

    Fix invalid username
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- ruyi-gitee-mirror.groovy
+++ ruyi-gitee-mirror.groovy
@@ -11,7 +11,7 @@ pipeline {
             }
 
             steps {
-                sh 'git clone https://${GITEE_APT_TOKEN_USR}:${GITEE_API_TOKEN_PSW}@gitee.com/${GITEE_REPO}.git orig --depth=1'
+                sh 'git clone https://${GITEE_APT_TOKEN}@gitee.com/${GITEE_REPO}.git orig --depth=1'
                 sh 'cd orig && git pull https://github.com/weilinfox/ruyi-gitee-mirror.git'
                 sh 'cd orig && git push'
                 sh 'cd orig && git push --tags'
